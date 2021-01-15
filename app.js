5db1bf4f14cde5f4a985c840610f6f1c90f1bd85
“arisa-hirata” <arisa.pirata@gmail.com>
2021-01-15 10:53:44 -0800

    Callback Abstraction

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 const request = require('postman-request');
+const geocode = require('./utils/geocode');
+// const url = 'http://api.weatherstack.com/current?access_key=a82f4d258896b5439e67cdbd68b4c0e4&query=37.8267,-122.4233&units=f';
 
-const url = 'http://api.weatherstack.com/current?access_key=a82f4d258896b5439e67cdbd68b4c0e4&query=37.8267,-122.4233&units=f';
+// request({ url: url, json: true }, (error, response) => {
+//     const temperature = response.body.current.temperature;
+//     const feelslike = response.body.current.feelslike;
+//     const desc = response.body.current.weather_descriptions[0];
 
-request({ url: url, json: true }, (error, response) => {
-    const temperature = response.body.current.temperature;
-    const feelslike = response.body.current.feelslike;
-    const desc = response.body.current.weather_descriptions[0];
+//     if (error) {
+//         console.log('Unable to connect to weather service!');
+//     } else if (response.body.error) {
+//         console.log('Unable to find location');
+//     } else {
+//         console.log(`${desc}. It is currently ${temperature} degrees out. It feels like ${feelslike}% degrees out.`);
+//     };
 
-    if (error) {
-        console.log('Unable to connect to weather service!');
-    } else if (response.body.error) {
-        console.log('Unable to find location');
-    } else {
-        console.log(`${desc}. It is currently ${temperature} degrees out. It feels like ${feelslike}% degrees out.`);
-    };
+// });
 
-});
+//  Geocoding
 
- Geocoding
+// const getUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJpc2FoaXJhdGEiLCJhIjoiY2tqeTA3eGNrMDh3aTJ3cDQzdW11aGd6dCJ9.gF1dj5V2ZFfvXeGBKmylAw&limit=1';
 
-const getUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJpc2FoaXJhdGEiLCJhIjoiY2tqeTA3eGNrMDh3aTJ3cDQzdW11aGd6dCJ9.gF1dj5V2ZFfvXeGBKmylAw&limit=1';
+// request({ url: getUrl, json: true }, (error, response) => {
+//     const coordinates = response.body.features[0].center;
 
-request({ url: getUrl, json: true }, (error, response) => {
-    const coordinates = response.body.features[0].center;
+//     if (error) {
+//         console.log('Unable to connect to weather services!');
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location');
+//     } else {
+//         console.log(`latutude is ${coordinates[0]}. Longitude is ${coordinates[1]}.`);
+//     };
+// });
 
-    if (error) {
-        console.log('Unable to connect to weather services!');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location');
-    } else {
-        console.log(`latutude is ${coordinates[0]}. Longitude is ${coordinates[1]}.`);
-    };
+
+geocode('Boston', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 });
